1a36fed26ad9f8d7a4d3a24e5a1fa19965a3cc58
Jim Rafferty <j.m.rafferty@swansea.ac.uk>
2022-03-22 11:23:41 +0000

    Added a definition of a server variable to hopefully fix a deployment error.

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 
+
 app = Dash(__name__, external_stylesheets=[dbc.themes.GRID])
+server = app.server
 
 app.layout = html.Div([
 
